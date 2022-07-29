8ab7aa33fb52bf1e5e55a634b21085e8fd8a1762
matthewfoxmi <matthewfoxmi@gmail.com>
2022-07-29 17:10:30 -0400

    Finished

---
+++ script.js
@@ -0,0 +1,31 @@
+let cart = [];
+
+let water = {Name: 'water', Price: 1.00}
+let coke = {Name: 'coke', Price: 2.00}
+let doritos = {Name: 'doritos', Price: 1.75}
+let tea = {Name: 'coke', Price: 1.25}
+let coffee = {Name: 'coke', Price: 2.00}
+
+function addItems(name, price)
+{
+    let item=
+    { Name: name,
+      Price: price
+    }
+    cart.push(item)
+    console.log('Added to your cart: ', item.Name)
+}
+
+function DisplayReceipt()
+{
+    let total = 0;
+    cart.forEach((element) => 
+    {  console.log(element.Name, element.Price)
+        total += element.Price
+    })
+    console.log("Your total is: ", total)
+    cart.length = 0
+}
+
+
+
